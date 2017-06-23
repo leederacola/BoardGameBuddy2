ed7ce28a9fe1d112e2d1a2b9def9eaaaf209cb9c
leederacola <rmgforce@gmail.com>
2017-06-23 17:09:33 -0400

    Entity Framework installed.  Game and User objects created.

---
+++ BGB2/BGB2/Models/Game.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BGB2.Models
+{
+    public class Game
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public int PlayerCountMin { get; set; }
+        public int PlayerCountMax { get; set; }
+        public int BestPlayerCount { get; set; }
+        public int PlayTimeMax { get; set; }        //measured in minuintes 
+        public double MyRating { get; set; }
+        public double GeekRating { get; set; }
+
+        public string DisplayTitle
+        //! Displays string Game.Title field
+        {
+            get
+            {
+                return Title;
+            }
+        }//end DisplayTitle
+    }
+}
