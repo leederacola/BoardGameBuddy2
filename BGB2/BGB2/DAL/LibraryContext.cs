3ed6721d2d4082928b5615b9aa82534483b9f6fa
leederacola <rmgforce@gmail.com>
2017-06-23 20:09:25 -0400

    Added Library Context and Initializer Files.  5 games in initilizer and 1 user "ryan"

---
+++ BGB2/BGB2/DAL/LibraryContext.cs
@@ -0,0 +1,22 @@
+﻿using BGB2.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace BGB2.DAL
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext() : base("LibraryContext")
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Game> Games { get; set; }
+
+        // prevents dups in table
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ BGB2/BGB2/DAL/LibraryInitializer.cs
@@ -0,0 +1,84 @@
+﻿using BGB2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BGB2.DAL
+{
+    public class LibraryInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<LibraryContext>
+    {
+        protected override void Seed(LibraryContext context)
+        {
+            var users = new List<User>
+            {
+            new User{Name="Ryan"},           
+            };
+
+            users.ForEach(u => context.Users.Add(u));
+            context.SaveChanges();
+
+            var games = new List<Game>
+            {
+                new Game()
+            {
+                ID = 1,
+                Title = "Blood Rage",
+                PlayerCountMin = 2,
+                PlayerCountMax = 5,
+                BestPlayerCount = 4,
+                PlayTimeMax = 90,
+                MyRating = 7.5,
+                GeekRating = 8.2,
+            },
+            new Game()
+            {
+                ID = 2,
+                Title = "Lost Cities",
+                PlayerCountMin = 2,
+                PlayerCountMax = 2,
+                BestPlayerCount = 2,
+                PlayTimeMax = 30,
+                MyRating = 8.1,
+                GeekRating = 7.1,
+            },
+            new Game()
+            {
+                ID = 3,
+                Title = "Dead of Winter: A Crossroads Game ",
+                PlayerCountMin = 2,
+                PlayerCountMax = 5,
+                BestPlayerCount = 4,
+                PlayTimeMax = 210,
+                MyRating = 7.5,
+                GeekRating = 7.9,
+            },
+            new Game()
+            {
+                ID = 4,
+                Title = "Cosmic Encounter",
+                PlayerCountMin = 3,
+                PlayerCountMax = 6,
+                BestPlayerCount = 5,
+                PlayTimeMax = 120,
+                MyRating = 9.0,
+                GeekRating = 7.6,
+            },
+            new Game()
+            {
+                ID = 5,
+                Title = "Codenames",
+                PlayerCountMin = 2,
+                PlayerCountMax = 8,
+                BestPlayerCount = 6,
+                PlayTimeMax = 15,
+                MyRating = 8,
+                GeekRating = 7.9,
+            }};
+
+            games.ForEach(g => context.Games.Add(g));
+            context.SaveChanges(); 
+        }
+    }
+}
+    
